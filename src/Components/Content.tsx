1660fa6673d80eff2d52da4dc54c20acecd86885
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2022-07-11 17:47:49 -0300

    174711072022

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -1,12 +1,85 @@
-import { Newtask } from "./Newtask";
-import { Tasks } from "./Tasks";
+import { ClipboardText, PlusCircle } from "phosphor-react"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { Task } from "./Task";
 
 export function Content() {
+  const [tasks, setTask] = useState([
+    'post mt bacana'
+  ]);
+
+  const [ newTaskText, setNewTaskText ] = useState('')
+
+  function handleCreateNewTask(event: FormEvent) {
+    event?.preventDefault();
+
+    setTask([...tasks, newTaskText])
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskText(event.target.value)
+  }
+
   return (
     <div className="flex justify-center ">
       <div className="w-[736px] mt-[-27px]">
-        <Newtask/>
-        <Tasks />
+        <form onSubmit={handleCreateNewTask} className="flex gap-2 mb-16">
+          <input
+            className="box-border w-full font-inter p-3 font-normal text-base text-person-300 rounded-lg bg-person-500 border-solid border-[1px] border-person-700"
+            type="text"
+            onChange={handleNewTaskChange}
+            name="newtask"
+            id="newtask"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button
+            className="flex bg-blueDark rounded-lg p-4 gap-2 font-inter font-bold text-person-100 justify-center items-center text-sm"
+          >
+            Criar
+            <PlusCircle size={16} weight="bold" />
+          </button>
+        </form>
+
+        <div>
+          <div className="flex justify-between mb-6">
+            <div className="flex items-center gap-2">
+              <p className="text-blue font-inter font-bold text-sm leading-[17px]">
+                Tarefas criadas
+              </p>
+              <span className="flex flex-col justify-center items-center  text-person-200 font-inter font-bold rounded-full text-xs bg-person-400 py-1 px-2.5">
+                5
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className="text-purple font-inter font-bold text-sm leading-[17px]">
+                Cuncluídas
+              </p>
+              <span className="flex flex-col justify-center items-center  text-person-200 font-inter font-bold rounded-full text-xs bg-person-400 py-1 px-2.5">
+                2 de 5
+              </span>
+            </div>
+          </div>
+
+          {tasks.length == 0 ? (
+            <div className="flex flex-col items-center justify-center py-16 border-t rounded-lg border-person-400 font-inter">
+              <div className="flex flex-col items-center justify-center text-person-300 text-base">
+                <ClipboardText size={56} weight="thin" />
+                <p className="font-bold mt-4">
+                  Você ainda não tem tarefas cadastradas
+                </p>
+                <p className="">Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-3">
+              {tasks.map(taskss => {                
+                return (
+                  <Task content={taskss}/>
+                )
+              })}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,12 +1,56 @@
+import { Trash } from "phosphor-react";
 
+interface tasksProps {
+  content: string
+}
 
-export function Task() {
+export function Task({content}:tasksProps) {
   return (
-    <div>
-      <label className="inline-flex items-center">
-        <input type="checkbox" className="form-checkbox" />
-        <span className="ml-2">Option 1</span>
+    <div className="flex p-4 gap-3 items-start bg-person-500 border border-person-400 rounded-lg">
+      <input
+        id="checkbox-task"
+        type="checkbox"
+        className="
+              peer
+              flex
+              appearance-none
+              w-5
+              h-5
+              border-2
+              border-blue
+              rounded-full
+              hover:border-blueDark
+              hover:bg-blueDark
+              hover:bg-opacity-20
+              checked:border-none
+              checked:bg-purpleDark
+              focus:outline-none
+              checked:hover:bg-purple
+            "
+      />
+
+      <label
+        htmlFor="checkbox-task"
+        className="
+            flex
+            font-inter
+            text-sm
+            font-normal
+            text-person-100
+            peer-checked:line-through
+            peer-checked:text-person-300
+            cursor-pointer
+            w-full
+          "
+      >
+        {content}
       </label>
-    </div>    
-  )
+      <a
+        href="#"
+        className="flex rounded-[4px] text-person-300 p-1 hover:bg-person-400 hover:text-danger"
+      >
+        <Trash size={24} />
+      </a>
+    </div>
+  );
 }
